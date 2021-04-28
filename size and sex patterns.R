9189d232c50a33926fdeed018689b738fa2e7c15
Matias Braccini <Matias.Braccini@fish.wa.gov.au>
2021-04-28 13:44:23 +0800

    Changed to OneNote wd

--- size and sex patterns.R
+++ size and sex patterns.R
@@ -18,7 +18,7 @@ do.paper=FALSE
 
 #DATA SECTION
 
-handl_OneDrive=function(x)paste('C:/Users/myb/OneDrive - Department of Primary Industries and Regional Development/Matias',x,sep='/')
+if(!exists('handl_OneDrive')) source('C:/Users/myb/OneDrive - Department of Primary Industries and Regional Development/Matias/Analyses/SOURCE_SCRIPTS/Git_other/handl_OneDrive.R')
 
 #Sharks data base  
 User="Matias"
